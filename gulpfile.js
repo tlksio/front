54e3c26bf79e195a7f920710983b5c22d3952301
Raül Pérez <repejota@gmail.com>
2015-02-27 04:07:30 +0100

    indentation

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,22 @@ var less = require('gulp-less');
 
 gulp.task('csslint', function () {
     gulp.src('./public/css/**/*.css')
-        .pipe(csslint())
-        .pipe(csslint.reporter());
+    .pipe(csslint())
+    .pipe(csslint.reporter());
 });
 
 gulp.task('jshint', function () {
-    gulp.src('./lib/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+    gulp.src(['./lib/**/*.js', './index.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
 gulp.task('less', function () {
     gulp.src('./src/less/**/*.less')
-        .pipe(less({
-            paths: []
-        }))
-        .pipe(gulp.dest('./public/css'));
+    .pipe(less({
+        paths: []
+    }))
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('default', ['jshint', 'less', 'csslint'], function () {
