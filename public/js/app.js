9ba07d7bb554d4b67ffd7c6b226fac54584b8f1d
jgil <jgil@trovit.com>
2015-03-20 22:35:37 +0100

    #30 Favorites by ajax request

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+    "use strict";
     $(".js-vote").click(function(){
           $.ajax({
               type: "GET",
@@ -16,4 +16,42 @@ $(document).ready(function(){
               },
          }); 
     });
+
+    $(".js-favorite, .js-unfavorite").click(function(){
+        var type = "";
+        if($(this).attr('class').indexOf("unfavorite")==-1) {
+           type = "up"; 
+        }
+        var command;
+        if(type=="up") {
+            command = "favorite";
+        } else {
+            command = "unfavorite";
+        }
+        $(this).attr("command", command);
+        $.ajax({
+             type: "GET",
+             url: "/talk/"+command+"/"+$(this).val(),
+             context: this,            
+             success: function(req) {
+             if( req.result=== true) {
+                  var spanDom = $('span', this);
+                  var numVotes = parseInt(spanDom.text());
+                  if ($(this).attr("command")=="favorite") {
+                      numVotes += 1;
+                      $('i', this).css('color', 'red');
+                      $(this).removeClass('js-favorite');
+                      $(this).addClass('js-unfavorite');
+                  } else {
+                      numVotes -= 1;
+                      $('i', this).css('color', 'black');
+                      $(this).removeClass('js-unfavorite');
+                      $(this).addClass('js-favorite');
+                  }
+                 spanDom.text(numVotes);
+               }
+             },
+         });
+    });
+
 });
