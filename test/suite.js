bfca5f14474bc74e831250c027676d90ad3a3f64
Raül Pérez <repejota@gmail.com>
2015-03-26 14:40:06 +0100

    Added FAQ UI tests, organized a little bit more all tests cases

--- test/suite.js
+++ test/suite.js
@@ -1,63 +1,7 @@
-// http://tlks.io/
-casper.test.begin('Testing tlks.io UI', 2, function(test) {
+var home = require('./tests/home');
+var about = require('./tests/about');
+var faq = require('./tests/faq');
 
-    var pageURL = "http://tlks.io/";
-
-    casper.start(pageURL);
-
-    casper.then(function() {
-        this.test.assert(this.getCurrentUrl() === pageURL, 'url is the one expected');
-    });
-
-    casper.then(function() {
-        this.test.assertHttpStatus(200, pageURL + ' is up');
-    });
-
-    casper.run(function() {
-        this.test.done();
-    });
-
-});
-
-// http://tlks.io/about
-casper.test.begin('Testing tlks.io : About UI', 2, function(test) {
-
-    var pageURL = "http://tlks.io/about";
-
-    casper.start(pageURL);
-
-    casper.then(function() {
-        this.test.assert(this.getCurrentUrl() === pageURL, 'url is the one expected');
-    });
-
-    casper.then(function() {
-        this.test.assertHttpStatus(200, pageURL + ' is up');
-    });
-
-    casper.run(function() {
-        this.test.done();
-    });
-
-});
-
-// http://tlks.io/faq
-casper.test.begin('Testing tlks.io : FAQ UI', 2, function(test) {
-
-    var pageURL = "http://tlks.io/faq";
-
-    casper.start(pageURL);
-
-    casper.then(function() {
-        this.test.assert(this.getCurrentUrl() === pageURL, 'url is the one expected');
-    });
-
-    casper.then(function() {
-        this.test.assertHttpStatus(200, pageURL + ' is up');
-    });
-
-    casper.run(function() {
-        this.test.done();
-        this.exit();
-    });
-
-});
+casper.test.begin('Testing tlks.io : Home', 2, home.testHome);
+casper.test.begin('Testing tlks.io : About', 2, about.testAbout);
+casper.test.begin('Testing tlks.io : FAQ UI', 2, faq.testFAQ);
