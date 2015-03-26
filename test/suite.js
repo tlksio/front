0eead0611058f99da5c517b9fe7708902b6ae1bc
Raül Pérez <repejota@gmail.com>
2015-03-26 15:11:36 +0100

    Added tests for privacy

--- test/suite.js
+++ test/suite.js
@@ -2,11 +2,13 @@ var home = require('./tests/home');
 var about = require('./tests/about');
 var faq = require('./tests/faq');
 var privacy = require('./tests/privacy');
+var terms = require('./tests/terms');
 
 casper.test.begin('Testing tlks.io : Home', 2, home.testHome),
 casper.test.begin('Testing tlks.io : About', 2, about.testAbout),
 casper.test.begin('Testing tlks.io : FAQ UI', 2, faq.testFAQ),
 casper.test.begin('Testing tlks.io : Privacy', 2, privacy.testPrivacy)
+casper.test.begin('Testing tlks.io : Terms', 2, terms.testTerms)
 
 casper.test.begin('Finish suite', 0, function() {
     casper.exit();
---
+++ test/tests/terms.js
@@ -0,0 +1,15 @@
+var pageURL = "http://tlks.io/terms";
+
+exports.testTerms = function(test) {
+    casper
+        .start(pageURL)
+        .then(function() {
+            test.assert(this.getCurrentUrl() === pageURL, 'url is the one expected');
+        })
+        .then(function() {
+            test.assertHttpStatus(200, pageURL + ' is up');
+        })
+        .run(function() {
+            test.done();
+        });
+};
