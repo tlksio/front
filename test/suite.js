13a195b7aafb7d1076705f204938c4d122fe5f88
Raül Pérez <repejota@gmail.com>
2015-03-26 15:08:42 +0100

    Added Privacy tests

--- test/suite.js
+++ test/suite.js
@@ -1,7 +1,13 @@
 var home = require('./tests/home');
 var about = require('./tests/about');
 var faq = require('./tests/faq');
+var privacy = require('./tests/privacy');
 
-casper.test.begin('Testing tlks.io : Home', 2, home.testHome);
-casper.test.begin('Testing tlks.io : About', 2, about.testAbout);
-casper.test.begin('Testing tlks.io : FAQ UI', 2, faq.testFAQ);
+casper.test.begin('Testing tlks.io : Home', 2, home.testHome),
+casper.test.begin('Testing tlks.io : About', 2, about.testAbout),
+casper.test.begin('Testing tlks.io : FAQ UI', 2, faq.testFAQ),
+casper.test.begin('Testing tlks.io : Privacy', 2, privacy.testPrivacy)
+
+casper.test.begin('Finish suite', 0, function() {
+    casper.exit();
+});
