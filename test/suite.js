01bd2817c342e7b9696c1e9a904d12309074a753
Raül Pérez <repejota@gmail.com>
2015-03-26 13:19:49 +0100

    Testing http://tlks.io for HTTP 200

--- test/suite.js
+++ test/suite.js
@@ -1,13 +1,23 @@
-var casper = require('casper').create();
+var siteName = "tlks.io";
 
-casper.start('http://tlks.io/', function() {
-        this.echo(this.getTitle());
+// http://tlks.io/
+casper.test.begin('Testing tlks.io UI', 2, function(test) {
+
+    var url = "http://tlks.io/";
+
+    casper.start(url);
+
+    casper.then(function() {
+        this.test.assert(this.getCurrentUrl() === url, 'url is the one expected');
+    });
+
+    casper.then(function() {
+        this.test.assertHttpStatus(200, siteName + ' is up');
+    });
+
+    casper.run(function() {
+        this.test.done();
+        this.exit();
+    });
+
 });
-
-/*
-casper.thenOpen('http://phantomjs.org', function() {
-        this.echo(this.getTitle());
-});
-*/
-
-casper.run();
