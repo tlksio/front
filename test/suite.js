2307e2272e30d580b62ca2006dc99045cd63a9e8
Raül Pérez <repejota@gmail.com>
2015-03-26 13:29:32 +0100

    Test About for HTTP 200

--- test/suite.js
+++ test/suite.js
@@ -17,6 +17,27 @@ casper.test.begin('Testing tlks.io UI', 2, function(test) {
 
     casper.run(function() {
         this.test.done();
+    });
+
+});
+
+// http://tlks.io/about
+casper.test.begin('Testing tlks.io : About UI', 2, function(test) {
+
+    var url = "http://tlks.io/about";
+
+    casper.start(url);
+
+    casper.then(function() {
+        this.test.assert(this.getCurrentUrl() === url, 'url is the one expected');
+    });
+
+    casper.then(function() {
+        this.test.assertHttpStatus(200, siteName + ' is up');
+    });
+
+    casper.run(function() {
+        this.test.done();
         this.exit();
     });
 
