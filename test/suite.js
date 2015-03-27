1d8df4ab6a13db6d344763e2492e85f3543c7786
Raül Pérez <repejota@gmail.com>
2015-03-27 01:15:14 +0100

    Added detailed talk view tests

--- test/suite.js
+++ test/suite.js
@@ -5,14 +5,16 @@ var privacy = require('./tests/privacy');
 var terms = require('./tests/terms');
 var activity = require('./tests/activity');
 var login = require('./tests/login');
+var talk = require('./tests/talk');
 
 casper.test.begin('Testing tlks.io : Home', 8, home.testHome);
 casper.test.begin('Testing tlks.io : About', 3, about.testAbout);
-casper.test.begin('Testing tlks.io : FAQ UI', 3, faq.testFAQ);
+casper.test.begin('Testing tlks.io : FAQ', 3, faq.testFAQ);
 casper.test.begin('Testing tlks.io : Privacy', 3, privacy.testPrivacy);
 casper.test.begin('Testing tlks.io : Terms', 3, terms.testTerms);
 casper.test.begin('Testing tlks.io : Activity', 3, activity.testActivity);
 casper.test.begin('Testing tlks.io : Login', 3, login.testLogin);
+casper.test.begin('Testing tlks.io : Talk detailed view', 3, talk.testTalk);
 
 casper.test.begin('Finish suite', 0, function() {
     casper.exit();
