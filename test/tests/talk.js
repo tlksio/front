a40c5ec5bea8fa87c2f01de304c970f6e6faca67
Raül Pérez <repejota@gmail.com>
2015-04-07 18:58:13 +0200

    Lint src

--- test/tests/talk.js
+++ test/tests/talk.js
@@ -1,17 +1,21 @@
-var pageURL = "http://tlks.io/talk/javascript-the-good-parts";
+/*jshint scripturl:true*/
+var pageURL = 'http://tlks.io/talk/javascript-the-good-parts';
 
 exports.testTalk = function(test) {
+    'use strict';
+
     casper
         .start(pageURL)
         .then(function() {
-            test.assert(this.getCurrentUrl() === pageURL, 'url is the one expected');
+            var msg = 'url is the one expected';
+            test.assert(this.getCurrentUrl() === pageURL, msg);
         })
         .then(function() {
             test.assertHttpStatus(200, pageURL + ' is up');
         })
         .then(function() {
-            var title = "JavaScript: The Good Parts | tlks.io";
-            test.assertTitle(title, "title is the one expected");
+            var title = 'JavaScript: The Good Parts | tlks.io';
+            test.assertTitle(title, 'title is the one expected');
         })
         .run(function() {
             test.done();
