8d020579e46ae627e91dfaf62f056f327ac1903e
Raül Pérez <repejota@gmail.com>
2015-04-07 19:01:36 +0200

    Lint src

--- test/tests/privacy.js
+++ test/tests/privacy.js
@@ -1,17 +1,20 @@
-var pageURL = "http://tlks.io/privacy";
+var pageURL = 'http://tlks.io/privacy';
 
 exports.testPrivacy = function(test) {
+    'use strict';
+
     casper
         .start(pageURL)
         .then(function() {
-            test.assert(this.getCurrentUrl() === pageURL, 'url is the one expected');
+            var msg = 'url is the one expected';
+            test.assert(this.getCurrentUrl() === pageURL, msg);
         })
         .then(function() {
             test.assertHttpStatus(200, pageURL + ' is up');
         })
         .then(function() {
-            var title = "tlks.io : Privacy policy";
-            test.assertTitle(title, "title is the one expected");
+            var title = 'tlks.io : Privacy policy';
+            test.assertTitle(title, 'title is the one expected');
         })
         .run(function() {
             test.done();
