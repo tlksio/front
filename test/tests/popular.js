e9aeb0c692fa5aa1e3370a85106207265a348919
Raül Pérez <repejota@gmail.com>
2015-04-07 20:52:23 +0200

    Ling src

--- test/tests/popular.js
+++ test/tests/popular.js
@@ -1,17 +1,20 @@
-var pageURL = "http://tlks.io/popular";
+var pageURL = 'http://tlks.io/popular';
 
 exports.testPopular = function(test) {
+    'use strict';
+
     casper
         .start(pageURL)
         .then(function() {
-            test.assert(this.getCurrentUrl() === pageURL, 'url is the one expected');
+            var msg = 'url is the one expected';
+            test.assert(this.getCurrentUrl() === pageURL, msg);
         })
         .then(function() {
             test.assertHttpStatus(200, pageURL + ' is up');
         })
         .then(function() {
-            var title = "tlks.io : Popular talks";
-            test.assertTitle(title, "title is the one expected");
+            var title = 'tlks.io : Popular talks';
+            test.assertTitle(title, 'title is the one expected');
         })
         .run(function() {
             test.done();
