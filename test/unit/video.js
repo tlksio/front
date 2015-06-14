e5ab002098666c5bb2bddd9c43c39b42aa482c36
Raül Pérez <raul.perez@r3labs.io>
2015-06-15 00:24:23 +0200

    Initial unit testing

---
+++ test/unit/video.js
@@ -0,0 +1,19 @@
+var should = require('should');
+var util = require('util');
+
+var video = require('../../lib/video.js');
+
+describe('Video', function() {
+    'use strict';
+
+    describe('get the type and the code from a video url', function() {
+
+        var result;
+
+        it('is a youtube video', function(done) {
+            should.notEqual(1, 2);
+            done();
+        });
+
+    });
+});
