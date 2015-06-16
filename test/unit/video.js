0df6a6b50287e2ed33d9bd9fd083b101894d0a48
Raül Pérez <raul.perez@r3labs.io>
2015-06-16 08:31:50 +0200

    Adding tests for parsing youtube video URLs

--- test/unit/video.js
+++ test/unit/video.js
@@ -4,7 +4,7 @@ var video = require('../../lib/video');
 describe('Video', function() {
     'use strict';
 
-    describe('https://www.youtube.com/watch?v=dQw4w9WgXcQ', function() {
+    describe('when i receive a youtube video url', function() {
 
         it('for a url www.youtube.com is a youtube video', function(done) {
             var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
@@ -16,7 +16,7 @@ describe('Video', function() {
 
 
         it('http://www.youtube.com/watch?v=dQw4w9WgXcQ', function(done) {
-            var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            var url = "http://www.youtube.com/watch?v=dQw4w9WgXcQ";
             var res = video.getCodeAndTypeFromRequest(url);
             should.equal(res[0], "dQw4w9WgXcQ");
             should.equal(res[1], "youtube");
@@ -24,7 +24,7 @@ describe('Video', function() {
         });
 
         it('http://youtube.com/watch?v=dQw4w9WgXcQ', function(done) {
-            var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            var url = "http://youtube.com/watch?v=dQw4w9WgXcQ";
             var res = video.getCodeAndTypeFromRequest(url);
             should.equal(res[0], "dQw4w9WgXcQ");
             should.equal(res[1], "youtube");
@@ -32,7 +32,7 @@ describe('Video', function() {
         });
 
         it('https://youtube.com/watch?v=dQw4w9WgXcQ', function(done) {
-            var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            var url = "https://youtube.com/watch?v=dQw4w9WgXcQ";
             var res = video.getCodeAndTypeFromRequest(url);
             should.equal(res[0], "dQw4w9WgXcQ");
             should.equal(res[1], "youtube");
@@ -40,6 +40,13 @@ describe('Video', function() {
         });
 
 
+        it('https://youtu.be/6DzSAaNQHR8', function(done) {
+            var url = "https://youtu.be/6DzSAaNQHR8";
+            var res = video.getCodeAndTypeFromRequest(url);
+            should.equal(res[0], "dQw4w9WgXcQ");
+            should.equal(res[1], "youtube");
+            done();
+        });
 
     });
 });
