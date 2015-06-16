89def779d625830f869d34f64ec3d0c051555cbe
Raül Pérez <raul.perez@r3labs.io>
2015-06-16 08:22:40 +0200

    Tested get youtube code and url from different youtube uris

--- test/unit/video.js
+++ test/unit/video.js
@@ -4,17 +4,42 @@ var video = require('../../lib/video');
 describe('Video', function() {
     'use strict';
 
-    describe('get the type and the code from a video url', function() {
+    describe('https://www.youtube.com/watch?v=dQw4w9WgXcQ', function() {
 
         it('for a url www.youtube.com is a youtube video', function(done) {
-
             var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
             var res = video.getCodeAndTypeFromRequest(url);
-            should.equal(res.code, "dQw4w9WgXcQ");
-            should.equal(res.type, "youtube");
-            should.notEqual(1, 2);
+            should.equal(res[0], "dQw4w9WgXcQ");
+            should.equal(res[1], "youtube");
             done();
         });
+
+
+        it('http://www.youtube.com/watch?v=dQw4w9WgXcQ', function(done) {
+            var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            var res = video.getCodeAndTypeFromRequest(url);
+            should.equal(res[0], "dQw4w9WgXcQ");
+            should.equal(res[1], "youtube");
+            done();
+        });
+
+        it('http://youtube.com/watch?v=dQw4w9WgXcQ', function(done) {
+            var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            var res = video.getCodeAndTypeFromRequest(url);
+            should.equal(res[0], "dQw4w9WgXcQ");
+            should.equal(res[1], "youtube");
+            done();
+        });
+
+        it('https://youtube.com/watch?v=dQw4w9WgXcQ', function(done) {
+            var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            var res = video.getCodeAndTypeFromRequest(url);
+            should.equal(res[0], "dQw4w9WgXcQ");
+            should.equal(res[1], "youtube");
+            done();
+        });
+
+
 
     });
 });
