5be32dd66dd9db605da5f85c1b48233d00543aca
Raül Pérez <raul.perez@r3labs.io>
2015-06-16 08:16:15 +0200

    Add a test for parsing youtube videos

--- test/unit/video.js
+++ test/unit/video.js
@@ -1,11 +1,17 @@
 var should = require('should');
+var video = require('../../lib/video');
 
 describe('Video', function() {
     'use strict';
 
     describe('get the type and the code from a video url', function() {
 
-        it('is a youtube video', function(done) {
+        it('for a url www.youtube.com is a youtube video', function(done) {
+
+            var url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            var res = video.getCodeAndTypeFromRequest(url);
+            should.equal(res.code, "dQw4w9WgXcQ");
+            should.equal(res.type, "youtube");
             should.notEqual(1, 2);
             done();
         });
